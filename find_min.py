4d6ea43d696fe0f1694adf801ff5995fc249b901
aisulubekbossynova <aisulu175@gmail.com>
2018-10-12 10:36:40 +0600

    finding minimum is updated

--- find_min.py
+++ find_min.py
@@ -2,38 +2,57 @@
 
 def get_min(a, b):
     """
-        return min number among a and b
+        return minimum among a and b
     """
+    if a>b:
+        return b
+    else:
+        return a
 
 
 def get_min_without_arguments():
     """
         raise TypeError exception with message
     """
+    raise TypeError('Something is happened')
 
 
 def get_min_with_one_argument(x):
     """
         return that value
     """
-
+    return x
 
 def get_min_with_many_arguments(*args):
     """
         return smallest number among args
     """
-
+    u = args[0]
+    for i in args:
+        if i<u:
+            u = i
+    return u
 
 def get_min_with_one_or_more_arguments(first, *args):
     """
         return smallest number among first + args
     """
+    u = first
+    for i in args:
+        if i<u:
+            u = i
+    return u
 
 
 def get_min_bounded(*args, low, high):
     """
         return smallest number among args bounded by low & high
     """
+    u = high
+    for i in args:
+        if i<u and low<i<high :
+            u = i
+    return u
 
 
 def make_min(*, low, high):
@@ -45,6 +64,11 @@ def make_min(*, low, high):
     """
 
     def inner(first, *args):
-        pass
+        u = high
+        for i in args:
+            if i<u and low<i<high:
+                u = i
+        return u
 
     return inner
+
