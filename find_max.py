de16ad4c922f34635f91708387d67c9d2c52b1b4
aisulubekbossynova <aisulu175@gmail.com>
2018-10-13 14:11:07 +0600

    added find_max.py and test for find_max

--- find_max.py
+++ find_max.py
@@ -1,29 +1,73 @@
 def get_max(a, b):
-    pass
+    """
+        return maximum among a and b
+    """
+    if a<b:
+        return b
+    else:
+        return a
 
 
 def get_max_without_arguments():
-    pass
+    """
+        raise TypeError exception with message
+    """
+    raise TypeError('Something is happened')
 
 
 def get_max_with_one_argument(a):
-    pass
+    """
+        return that value
+    """
+    return a
 
 
 def get_max_with_many_arguments(*args):
-    pass
+    """
+        return biggest number among args
+    """
+    u = args[0]
+    for i in args:
+        if i>u:
+            u = i
+    return u
 
 
 def get_max_with_one_or_more_arguments(first, *args):
-    pass
+    """
+        return biggest number among first + args
+    """
+    u = first
+    for i in args:
+        if i>u:
+            u = i
+    return u
+
 
 
 def get_max_bounded(*args, low, high):
-    pass
+    """
+        return biggest number among args bounded by low & high
+    """
+    u = high
+    for i in args:
+        if i>u and low<i<high :
+            u = i
+    return u
 
 
 def make_max(*, low, high):
+    """
+        return inner function object which takes at last one argument
+        and return biggest number amount it's arguments, but if the
+        biggest number is bigger than the 'high' which given as required
+        argument the inner function has to return it.
+    """
     def inner(first, *args):
-        pass
-
+        u = high
+        for i in (first,) + args:
+            if i>u and low<i<high:
+                u = i
+        return u
+    
     return inner
--- tests/find_max_test.py
+++ tests/find_max_test.py
@@ -1,24 +1,41 @@
 from unittest import TestCase
-
+import find_max as f
 
 class FindMaxTest(TestCase):
     def test_get_max(self):
-        pass
+        result = f.get_max(3,21)
+        self.assertEqual(result, 21)
 
     def test_get_max_without_arguments(self):
-        pass
+        self.assertRaises(TypeError, f.get_max_without_arguments)
 
     def test_get_max_with_one_argument(self):
-        pass
+        result = f.get_max_with_one_argument(9)
+        self.assertEqual(result, 9)
 
     def test_get_max_with_many_arguments(self):
-        pass
+        result = f.get_max_with_many_arguments(1, 2, 3, 4)
+        self.assertEqual(4, result)
 
     def test_get_max_with_one_or_more_arguments(self):
-        pass
+        first = 9
+        array = [48, 4, 12]
+        result = f.get_max_with_one_or_more_arguments(first, *array)
+        self.assertEqual(48, result)
 
     def test_get_max_bounded(self):
-        pass
+        kwargs = {
+            'low': 0,
+            'high': 34
+        }
+        result = f.get_max_bounded(51, 9, 20, **kwargs)
+        self.assertEqual(34, result)
 
     def test_make_max(self):
-        pass
+        bounded_max = f.make_max(low=0, high=255)
+        self.assertEqual(True, callable(bounded_max))
+        
+        if callable(bounded_max):
+            result = bounded_max(-5, 12, 43)
+            self.assertEqual(255, result)
+
